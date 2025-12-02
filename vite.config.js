3df841127f34fac7131ad514be278ecfde1a9fe1
dexterR35 <pusaw202@gmail.com>
2025-12-02 15:25:46 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use absolute paths for Vercel
+  base: '/', // Use absolute paths
   preview: {
     // Configure preview server for SPA routing
     port: 4173,
